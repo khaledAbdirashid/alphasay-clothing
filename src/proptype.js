0b08a676cc14b9a6790a9d96fd9d14ddfef5eae9
khaledAbdirashid <khaledabdirashid12@gmail.com>
2024-05-22 19:29:26 -0400

    added fonts and fixed a proptype bug

--- src/proptype.js
+++ src/proptype.js
@@ -5,12 +5,11 @@ export const categoriesPropTypes = PropTypes.arrayOf(
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     imageUrl: PropTypes.string.isRequired,
-  })
+  }).isRequired
 );
 
-export const categoryItemPropTypes = {
-  category: PropTypes.shape({
-    imageUrl: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-  }).isRequired,
-};
+export const categoryItemPropTypes = PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+}).isRequired;
