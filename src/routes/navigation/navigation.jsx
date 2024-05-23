100a7c0b03f18305d96b061ba596b1ccc365a878
khaledAbdirashid <khaledabdirashid12@gmail.com>
2024-05-23 10:54:44 -0400

    Added React Router with routes for home and shop, also implemented navigation component

---
+++ src/routes/navigation/navigation.jsx
@@ -0,0 +1,23 @@
+import { Outlet, Link } from "react-router-dom";
+import "./navigation.scss";
+import crownIcon from "../../assets/crown.svg";
+
+function Navigation() {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img src={crownIcon} alt="a crown logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+}
+
+export default Navigation;
---
+++ src/routes/shop/shop.jsx
@@ -0,0 +1,5 @@
+function Shop() {
+  return <h1>This is the shop page</h1>;
+}
+
+export default Shop;
